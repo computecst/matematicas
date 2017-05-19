a343be7c7b5260927ea90166f79feefb86b5afa6
Santiago <santiago25.10infor@gmail.com>
2017-05-19 02:57:46 -0500

    error

--- mate.sh
+++ mate.sh
@@ -11,11 +11,8 @@ PHP=`apt-get install php7.0 php7.0-common php7.0-dba php7.0 -y >> /dev/null`
 VIRTUALENVIRONMENT="/etc/apache2/sites-available/"
 NAME_VIRTUALENVIRONMENT="webapplication"
 URL_WEB_APP="/var/www/html/"$NAME_VIRTUALENVIRONMENT
-#echo $URL_WEB_APP
-#exit
 URL_CODEIGNITER="wget -O code.zip https://github.com/bcit-ci/CodeIgniter/archive/3.1.4.zip"
 
-#DOWNLOAD_CODEIGNITER="wget -O code.zip $URL_CODEIGNITER | unzip code.zip >> /dev/null | cd CodeIgniter-3.1.4 | mv * ./../ | cd .. | rm -r CodeIgniter-3.1.4"
 
 ##### Argumentos
 VERBOSE=$1
@@ -26,8 +23,8 @@ ZIP=`apt-get install zip -y >> /dev/null`
 $ZIP
 
 ### funciones
-function config_virtual_environment {
-	echo -e "<VirtualHost *:80>
+config_virtual_environment {
+	echo  "<VirtualHost *:80>
         ServerName $NAME_VIRTUALENVIRONMENT".loc"
         ServerAlias "www."$NAME_VIRTUALENVIRONMENT".loc"
         ServerAdmin webmaster@localhost
@@ -39,7 +36,6 @@ function config_virtual_environment {
 
 	# grep -c --> cuenta numero de coincidencias
 	ETC_HOST=`awk '{print }' /etc/hosts | grep -c webapplication`
-	echo $ETC_HOST
 
 	if [ $ETC_HOST -le 0 ]; then
 		echo -e "127.0.0.1 \t$NAME_VIRTUALENVIRONMENT.loc" >> "/etc/hosts"
@@ -55,7 +51,6 @@ function config_virtual_environment {
 		fi
 	fi
 }
-
 
 
 
@@ -77,10 +72,8 @@ if $APACHE; then
 	echo -e "apache: True \t datetime: $DIA $HORA" >> install.log
 	
 	# creando entorno virtual
-	if [ -d $VIRTUALENVIRONMENT ]
-	then
-		if [ -f $NAME_VIRTUALENVIRONMENT ]
-		then
+	if [ -d $VIRTUALENVIRONMENT ]; then
+		if [ -f $VIRTUALENVIRONMENT$NAME_VIRTUALENVIRONMENT".conf" ]; then
 			echo "YA existe el archivo $NAME_VIRTUALENVIRONMENT en la ruta $VIRTUALENVIRONMENT, por tanto no se puede crear el entorno virtual"
 		else
 			if [ -d $URL_WEB_APP ]; then
@@ -96,6 +89,7 @@ if $APACHE; then
                         	#pwd
 				$URL_CODEIGNITER
 				unzip code.zip >> /dev/null
+				rm -r code.zip
                         	cd "CodeIgniter-3.1.4"
                         	mv * ./../
                         	cd ..
@@ -132,20 +126,20 @@ else
 fi
 
 # instalando postgresql
-if $POSTGRESQL; then
-        echo -e "postgresql: True \t datetime: $DIA $HORA" >> install.log
-else
-        echo -e "postgresql: False \t datetime: $DIA $HORA" >> install.log
-        echo "error para instalar postgresql, no se puede proceder" 
-        exit
-fi
+#if $POSTGRESQL; then
+#        echo -e "postgresql: True \t datetime: $DIA $HORA" >> install.log
+#else
+#        echo -e "postgresql: False \t datetime: $DIA $HORA" >> install.log
+#        echo "error para instalar postgresql, no se puede proceder" 
+#        exit
+#fi
 
 # instalando mysql
-if $MYSQL; then
-        echo -e "mysql: True \t datetime: $DIA $HORA" >> install.log
-else
-        echo -e "mysql: False \t datetime: $DIA $HORA" >> install.log
-        echo "error para instalar mysql, no se puede proceder" 
-        exit
-fi
+#if $MYSQL; then
+#        echo -e "mysql: True \t datetime: $DIA $HORA" >> install.log
+#else
+#        echo -e "mysql: False \t datetime: $DIA $HORA" >> install.log
+#        echo "error para instalar mysql, no se puede proceder" 
+#        exit
+#fi
 
